bb9966e07b81750ad954db021479e87c75d47a73
Constantine <tmail99@gmail.com>
2025-06-25 14:46:45 +0300

    подправил логику

--- main.go
+++ main.go
@@ -26,8 +26,8 @@ func newResident(id string, name string) resident {
 func main() {
 	// Может потом пойму почему в метод с одним параметром,
 	// нужно отправлять два!?
-	user, _ := resident.GetUserByID(newResident("2", "Kid"), "2")
-	fmt.Println(*user)
+	//user, _ := resident.GetUserByID(newResident("2", "Kid"), "2")
+	//fmt.Println(*user)
 
 	// Здесь главное!
 	// Обращаюсь к конструктору service.NewUserService, принимающему параметром
@@ -41,23 +41,30 @@ func main() {
 	// Т.к. он должен в точности реализовывать методы интерфейса (заданные не мной!),
 	// то метод GetUserByID возвращает тип *models.User ,
 	// а уже его я привожу- resident(*user)
-	hz := service.NewUserService(resident(*user))
-	fmt.Println(hz)
+	//service.NewUserService(resident(*user))
+	user := *service.NewUserService(newResident("2", "Kid"))
+	// обращаюсь к service.GetUser
+	fmt.Println(user.GetUser("2"))
+	// обращаюсь к service.DeleteUser
+	fmt.Println(user.DeleteUser("2"))
+
 	// Пока (13.06.25) я получаю
 	// &{{2 Kid}}
 	// Но это не так важно!
 	// Главное- я получил данные из стороннего кода, не нарушая его.
 	// Теперь тесты с моками должны быть более понятны!
-
-	// Включение или нет, вызова DeleteUser сути дела не меняет.
-	// Главное- мой объект resident здесь реализует второй метод
-	// интерфейса internal.UserRepository
-	resident.DeleteUser(resident(*user), "2")
+	// get, _ := resident.GetUserByID(resident(*&user.DeleteUser), "2")
+	// fmt.Println("найдена запись:", *get)
+	// // Включение или нет, вызова DeleteUser сути дела не меняет.
+	// // Главное- мой объект resident здесь реализует второй метод
+	// // интерфейса internal.UserRepository
+	// resident.DeleteUser(resident(*user), "2")
 
 }
 
-// Первый метод интерфейса internal.UserRepository
+// Первый метод интерфейса internal.UserRepository , нахожу
 func (s resident) GetUserByID(id string) (*models.User, error) {
+	fmt.Println("Я GetUserByID , вызываюсь из service.GetUser и получил:")
 
 	person := models.User{
 		ID:   s.ID,
@@ -70,6 +77,7 @@ func (s resident) GetUserByID(id string) (*models.User, error) {
 // Второй метод интерфейса internal.UserRepository
 func (s resident) DeleteUser(id string) error {
 	// Стираю данные в структуре
+	fmt.Println("Я DeleteUser, вызываюсь из service.DeleteUser и удаляю запись:", s)
 	s.ID = ""
 	s.Name = ""
 	fmt.Println(s)
